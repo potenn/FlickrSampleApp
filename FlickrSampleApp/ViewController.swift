84545f71eac97b91016dfc26f60c9bbf6b147849
potenn <poten1129@gmail.com>
2017-02-12 15:31:31 +0900

    add comments.

--- FlickrSampleApp/ViewController.swift
+++ FlickrSampleApp/ViewController.swift
@@ -25,8 +25,9 @@ class ViewController: UIViewController {
     @IBAction func timeIntervalSliderChanged(_ sender: Any) {
         let currentValue = Int(self.timeIntervalSlider.value)
         self.timeIntervalLabel.text = "\(currentValue) Seconds"
-        
     }
+    
+    // MARK: - Variable
     
     /*
      if you use all images from feeds of photos_public and canContinueLoad is true, you can get new images from feeds of photos_public.
@@ -43,12 +44,16 @@ class ViewController: UIViewController {
      */
     var count : Int = 0
     
-    
-    // index of flickrImageArray
+    /*
+    index of flickrImageArray
+     */
     var idx : Int = 0
     
-    // image Array for Flickr Images
+    /* 
+     image Array for Flickr Images
+    */
     var flickrImageArray : [String] = []
+    
     
     /*
      Feeds of photos_public API
@@ -56,6 +61,8 @@ class ViewController: UIViewController {
      */
     let serverURL = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"
     
+    
+    // MARK: - viewDidLoad()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,25 +76,74 @@ class ViewController: UIViewController {
     
     // MARK: - Start Digital Photo Album
     @IBAction func startButtonClicked(_ sender: Any) {
-        self.playImagesOfFlickr()
+        
+        self.playImagesOfFlickr(showAnimationFunction: showAnimation, hideAnimationFunction: hideAnimation)
     }
     
+    // MARK: - Animation
+    
+    /*
+     You can write animation if you want to show using other animations
+     default : Fade-In
+     */
+    func showAnimation() {
+        
+        //Fade in
+        UIView.animate(withDuration: 2.0, animations: {() -> Void in
+            
+            self.flickrImageView.alpha = 1.0
+            self.idx = self.idx + 1
+            
+        }, completion: {(_ finished: Bool) -> Void in
+            print("Show Image")
+        })
+        
+    }
+    
+    /*
+     You can write animation if you want to hide using other animations
+     default : Fade-Out
+     */
+    func hideAnimation() {
+        //Fade out
+        UIView.animate(withDuration: 2.0, animations: {() -> Void in
+            print("Fade out Index \(self.index)");
+            self.flickrImageView.alpha = 0.0
+        }, completion: { _ in
+            print("Hide Image Animation Completed")
+        })
+    }
     
     // MARK: - Show Image Start
-    func playImagesOfFlickr() -> Void{
+    
+    /**
+     show Images from Flickr API.
+     
+     - param showAnimationFunction : func task1()
+     - param hideAnimationFunction : func task2()
+     
+     - return : void
+     */
+
+    func playImagesOfFlickr(showAnimationFunction:@escaping (Void)->Void, hideAnimationFunction:@escaping (Void)->Void){
         
         let timerInvalidate: Bool = false
         
         DispatchQueue.global(qos: .default).async(execute: {() -> Void in
             
             while !timerInvalidate {
-
+                
                 DispatchQueue.global(qos: .background).async(execute: {() -> Void in
                     
-                    print("Thread Global")
+                    print("Thread Global Background")
                     
+                    /*
+                     use all Images in flickrImageArray, so
+                     you must get new images from Flickr Images.
+                     */
                     if self.idx >= self.count{
-                        print("refreshFlickrImage")
+                        
+                        print("RefreshFlickrImage")
                         self.idx = 0
                         self.canContinueLoad = true
                         self.refreshFlickrImage()
@@ -97,49 +153,49 @@ class ViewController: UIViewController {
                         
                         if self.canShowImage{
                             
-
                             if self.count > self.idx{
-
-                                    self.flickrImageView.sd_setImage(with: URL(string: self.flickrImageArray[self.idx])!, completed:{ (image, error, cacheType, imageURL) in
-                                    
-                                        // Perform operation.
-                                        
-                                        //fade in
-                                        UIView.animate(withDuration: 2.0, animations: {() -> Void in
-                                            
-                                            self.flickrImageView.alpha = 1.0
-                                            self.idx = self.idx + 1
-                                            
-                                        }, completion: {(_ finished: Bool) -> Void in
-                                            print("Show Image")
-                                        })
-                                        
-                                    })
                                 
+                                self.flickrImageView.sd_setImage(with: URL(string: self.flickrImageArray[self.idx])!, completed:{ (image, error, cacheType, imageURL) in
+                                    
+                                    /*
+                                     completed get Image from serverURL, Perform Show Animation.
+                                     */
+                                    showAnimationFunction()
+                                })
                             }
                         }else{
-                            print("Not Shown yet")
+                            print("Not Ready for Showing Images")
                         }
                     }
                 })
- 
+                
                 print("Global Thread Sleep \(self.timeIntervalSlider.value) seconds")
-                Thread.sleep(forTimeInterval: TimeInterval(self.timeIntervalSlider.value + 2.0))
+                /*
+                 Thread.sleep plus 4.0
+                 Because each Animation(Show, Hide) has 1.0 seconds.
+                 (show : 2 seconds, hide : 2 seconds)
+                 */
+                Thread.sleep(forTimeInterval: TimeInterval(self.timeIntervalSlider.value + 4.0))
                 
-                //fade out
-                UIView.animate(withDuration: 1.0, animations: {() -> Void in
-                    print("Fade out Index \(self.index)");
-                    self.flickrImageView.alpha = 0.0
-                }, completion: { _ in })
-                
-                
+                //Fade out
+                hideAnimationFunction()
             }
         })
     }
     
-    // MARK: - Get images from serverURL(https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1)
     
-    func refreshFlickrImage() -> Void{
+
+    // MARK: - Get images from serverURL(https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1)
+    /**
+     
+     get new Images from Flickr API.
+     
+     - param : void
+
+     - return : void
+     
+     */
+    func refreshFlickrImage() {
         
         if canContinueLoad{
             
@@ -150,7 +206,7 @@ class ViewController: UIViewController {
                 case .success(let JSON):
                     
                     let response = JSON as! NSDictionary
-                    let flickrDataArray =  response.object(forKey: "items") as! NSArray
+                    let flickrDataArray = response.object(forKey: "items") as! NSArray
                     
                     if self.count == self.flickrImageArray.count{
                         self.flickrImageArray.removeAll()
@@ -186,6 +242,8 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    // MARK: - didReceiveMemoryWarning
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
